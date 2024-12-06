c01ad9bf5fb1412baba9e9cbd9dd848150934537
Tom Jemmett <thomas.jemmett@nhs.net>
2024-12-06 08:59:38 +0000

    refactor day 6
    improves runtime from ~60s to 2s. Instead of checking all the positions in the grid, just check the positions which are visited in part 1

--- src/Day06.hs
+++ src/Day06.hs
@@ -26,22 +26,22 @@ day06 :: AOCSolution
 day06 input = show <$> ([part1, part2] <*> pure (parseInput input))
 
 part1 :: (M.HashMap Point2d Char, Point2d) -> Int
-part1 = fromMaybe 0 . move S.empty North
+part1 = length . fromMaybe [] . move S.empty North
 
 part2 :: (M.HashMap Point2d Char, Point2d) -> Int
 part2 (grid, start) = countTrue isNothing checkGrids
   where
-    positionsToCheck = M.keys $ M.filter (== '.') grid
+    Just positionsToCheck = move S.empty North (grid, start)
     checkGrids = map run positionsToCheck
     run k = move S.empty North (M.insert k '#' grid, start)
 
-move :: S.HashSet (Point2d, String) -> Direction -> (M.HashMap Point2d Char, Point2d) -> Maybe Int
+move :: S.HashSet (Point2d, String) -> Direction -> (M.HashMap Point2d Char, Point2d) -> Maybe [Point2d]
 move visited dir (grid, pos)
   | S.member (pos, show dir) visited = Nothing
   | otherwise = case M.lookup pos' grid of
       Just '#' -> move visited' (turn90 dir) (grid', pos)
       Just _ -> move visited' dir (grid', pos')
-      Nothing -> Just $ M.size $ M.filter (== 'X') grid'
+      Nothing -> Just $ M.keys $ M.filter (== 'X') grid'
   where
     pos' = newPos pos dir
     grid' = M.insert pos 'X' grid
