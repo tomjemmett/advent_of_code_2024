3d364e1166d5a71dd721113d26acd2d87478f989
Tom Jemmett <thomas.jemmett@nhs.net>
2024-12-11 09:45:55 +0000

    adds solution for day 11

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,11 +1,32 @@
 module Day11 (day11, day11TestInput) where
 
 import Common
-import Text.Parsec qualified as P
-import Text.Parsec.String (Parser)
+import Data.HashMap.Strict qualified as M
+import Data.List (splitAt)
 
 day11TestInput :: String
-day11TestInput = ""
+day11TestInput = "125 17"
 
 day11 :: AOCSolution
-day11 input = []
+day11 input = show . sum . M.elems . (r !!) <$> [25, 75]
+  where
+    i = parseInput input
+    r = iterate blink i
+
+parseInput :: String -> M.HashMap Int Int
+parseInput = M.fromListWith (+) . map ((,1) . read) . words
+
+blink :: M.HashMap Int Int -> M.HashMap Int Int
+blink = M.foldrWithKey' f mempty
+  where
+    f stone count map = foldr (flip (M.insertWith (+)) count) map $ rule stone
+
+rule :: Int -> [Int]
+rule stone
+  | stone == 0 = [1]
+  | even stoneLen = map read $ untuplify2 $ splitAt n stoneStr
+  | otherwise = [stone * 2024]
+  where
+    stoneStr = show stone
+    stoneLen = length stoneStr
+    n = stoneLen `div` 2
--- test/Day11Spec.hs
+++ test/Day11Spec.hs
@@ -5,7 +5,7 @@ import SpecHelper
 spec :: Spec
 spec = describe "Day 11" $ do
   it "Sample" $ do
-    day11 day11TestInput `shouldBe` []
+    day11 day11TestInput `shouldBe` ["55312", "65601038650482"]
 
   it "Actual" $ do
     withFile
@@ -13,5 +13,5 @@ spec = describe "Day 11" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day11 actualInput `shouldBe` []
+          day11 actualInput `shouldBe` ["224529", "266820198587914"]
       )
