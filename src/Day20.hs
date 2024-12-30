02fd23535480195a872e18640a76566cd5edfcf4
Tom Jemmett <tom.jemmett@gmail.com>
2024-12-30 00:23:43 +0000

    performance improvements
    reduces memory usage during runtime by using strict datastruces,
    Data.Sequence, and using a fold instead of filtering

--- src/Day20.hs
+++ src/Day20.hs
@@ -1,12 +1,14 @@
 module Day20 (day20, day20TestInput) where
 
 import Common
-import Control.Monad.RWS
+import Control.Monad.RWS.Strict
 import Data.Function (on)
 import Data.HashMap.Strict qualified as M
 import Data.HashSet qualified as S
 import Data.Hashable (Hashable)
 import Data.List (find, sortBy)
+import Data.Sequence qualified as SQ
+import Data.Sequence ((><), Seq((:<|)))
 
 type Grid = M.HashMap Point2d Bool
 
@@ -39,8 +41,7 @@ day20 input = show . solve <$> [(== 2), (< 21)]
     (g, s) = parseInput input
     distances = floodFill g s
     ds = sortBy (compare `on` snd) $ M.toList distances
-    cs = map getCheat $ concat $ combinations ds
-    solve f = countTrue (((>= 100) . snd) <&> (f . fst)) cs
+    solve f = combinations (validCheat f) ds
 
 parseInput :: String -> (Grid, Point2d)
 parseInput input = (M.map (/= '#') $ M.fromList i, s)
@@ -52,27 +53,27 @@ parseInput input = (M.map (/= '#') $ M.fromList i, s)
     Just s = fst <$> find ((== 'S') . snd) i
 
 floodFill :: Grid -> Point2d -> M.HashMap Point2d Int
-floodFill g s = M.insert s 0 $ snd $ execRWS (go [(s, 1)]) g (S.singleton s)
+floodFill g s = M.insert s 0 $ snd $ execRWS (go $ SQ.singleton (s, 1)) g (S.singleton s)
   where
-    go :: [(Point2d, Int)] -> RWS Grid Distances Visited ()
-    go [] = pure ()
-    go ((p, d) : ps) = do
+    go :: SQ.Seq (Point2d, Int) -> RWS Grid Distances Visited ()
+    go SQ.Empty = pure ()
+    go ((p, d) :<| ps) = do
       g <- ask
       v <- get
       ps' <- forM (filter (notMember v <&> (g M.!)) $ point2dNeighbours p) $ \n -> do
         modify $ S.insert n
-        tell $ M.singleton n d
+        tell $ M.singleton n $! d
         pure (n, succ d)
-      go (ps' ++ ps)
+      go (ps >< SQ.fromList ps')
 
 notMember :: (Hashable a) => S.HashSet a -> a -> Bool
 notMember s = not . (`S.member` s)
 
-getCheat :: ((Point2d, Int), (Point2d, Int)) -> (Int, Int)
-getCheat ((ip, id), (jp, jd)) =
-  let d = manhattanDistance ip jp
-   in (d, jd - id - d)
+validCheat :: (Int -> Bool) -> ((Point2d, Int), (Point2d, Int)) -> Bool
+validCheat f ((ip, id), (jp, jd)) = f d && (jd - id - d) >= 100
+  where
+    d = manhattanDistance ip jp
 
-combinations :: [a] -> [[(a, a)]]
-combinations xs | length xs < 2 = []
-combinations (x : xs) = map (x,) xs : combinations xs
+combinations :: ((a, a) -> Bool) -> [a] -> Int
+combinations _ xs | length xs < 2 = 0
+combinations f (x:xs) = countTrue (f . (x,)) xs + combinations f xs
