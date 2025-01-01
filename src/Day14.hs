504c45ccab0127bc340452bd6cee08f6433fdac5
Tom Jemmett <tom.jemmett@gmail.com>
2025-01-01 22:33:32 +0000

    performance improvements for day 14

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,7 +1,9 @@
 module Day14 (day14, day14TestInput) where
 
 import Common
-import Data.List (elemIndex, group, sort)
+import Data.Function (on)
+import Data.List (group, sort, maximumBy)
+import Data.IntMap qualified as IM
 import Text.Parsec qualified as P
 import Text.Parsec.String (Parser)
 
@@ -24,19 +26,26 @@ day14TestInput =
   \"
 
 day14 :: AOCSolution
-day14 input = show <$> [part1, part2]
+day14 input = show <$> ([part1, part2] <*> pure wh <*> pure robots)
   where
     i = parseInput input
-    wh@(w, h) = if input == day14TestInput then (11, 7) else (101, 103)
-    robots = take (w * h) $ iterate (moveRobots wh) i
+    wh = if input == day14TestInput then (11, 7) else (101, 103)
+    robots = iterate (moveRobots wh) i
+
+part1 :: (Int, Int) -> [[Robot]] -> Int
+part1 wh robots = cycle safetyScores !! 100
+  where
     safetyScores = map (countQuadrants wh) robots
-    part1 = cycle safetyScores !! 100
-    -- two assumptions for part 2:
-    -- 1. our answer must be in the first 101 * 103 iterations, as the patterns will repeat.
-    --    both the height and the width are prime numbers, so any addition will cycle in these fields
-    -- 2. the safety score from part 1 might be a hint that our answer will be a minimum value for
-    --    the safety score
-    Just part2 = elemIndex (minimum safetyScores) safetyScores
+
+part2 :: (Int, Int) -> [[Robot]] -> Int
+part2 (w, h) robots = r
+  where
+    (xss, yss) = unzip $ map (unzip . map fst) robots
+    getMax = fst . maximumBy (compare `on` snd) . zip [0..] . map fn
+    fn = maximum . IM.elems . IM.fromListWith (+) . map (,1)
+    x = getMax $ take w xss
+    y = getMax $ take h yss
+    (r:_) = dropWhile ((/= y) . (`mod` h)) $ iterate (+w) x
 
 parseInput :: String -> [Robot]
 parseInput = parse (P.many parseLine)
@@ -69,14 +78,3 @@ moveRobots (width, height) = map moveRobot
     wrapRobot :: Point2d -> Point2d
     wrapRobot (px, py) = (px `mod` width, py `mod` height)
 
--- debug function to help check part 2
-printGrid :: Point2d -> [Robot] -> IO ()
-printGrid (width, height) robots = do
-  let rs = map fst robots
-  putStrLn $
-    unlines $
-      [ [ if (x, y) `elem` rs then '#' else ' '
-          | x <- [0 .. width - 1]
-        ]
-        | y <- [0 .. height - 1]
-      ]
