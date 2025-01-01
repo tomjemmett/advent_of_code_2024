e8359297f2030c5add72e7625d3f1c337c143bab
Tom Jemmett <tom.jemmett@gmail.com>
2025-01-01 23:13:14 +0000

    performance improvements for day 18

--- src/Day18.hs
+++ src/Day18.hs
@@ -2,13 +2,8 @@ module Day18 (day18, day18TestInput) where
 
 import Algorithm.Search (aStar, pruning)
 import Common
-import Data.HashMap.Strict qualified as M
 import Data.HashSet qualified as S
-import Data.List (inits)
-
-type Memory = S.HashSet Point2d
-
-type Memories = M.HashMap Int Memory
+import Text.Parsec qualified as P
 
 day18TestInput :: String
 day18TestInput =
@@ -42,18 +37,21 @@ day18TestInput =
 day18 :: AOCSolution
 day18 input = [show p1, p2]
   where
-    (memories, size) = parseInput input
+    memories = parseInput input
+    size = maximum $ map snd memories
     p1T = if size == 6 then 12 else 1024
-    Just p1 = findPath size (memories M.! p1T)
+    Just p1 = findPath size (S.fromList $ take p1T memories)
     p2 = lines input !! part2 size memories
 
-parseInput :: String -> (Memories, Int)
-parseInput input = (M.fromList $ zip [0 ..] is, maximum $ map snd i)
+parseInput :: String -> [Point2d]
+parseInput = parse (P.many1 p)
   where
-    !i = map (\xs -> read $ ('(' : xs) ++ ")") $ lines input
-    !is = map S.fromList $ inits i
+    p = do
+      x <- number <* P.char ','
+      y <- number
+      pure (x, y)
 
-findPath :: Int -> Memory -> Maybe Int
+findPath :: Int -> (S.HashSet Point2d) -> Maybe Int
 findPath size memory = fst <$> aStar next dist (dist end) (== end) (0, 0)
   where
     end = (size, size)
@@ -63,15 +61,16 @@ findPath size memory = fst <$> aStar next dist (dist end) (== end) (0, 0)
       where
         fn (x, y) = or [x < 0, y < 0, x > size, y > size, (x, y) `S.member` memory]
 
-part2 :: Int -> Memories -> Int
-part2 size memories = go 0 n
+part2 :: Int -> [Point2d] -> Int
+part2 size memories = go 1024 n
   where
-    n = maximum $ M.keys memories
+    n = length memories
     go s e
       | s > e = e
       | s == e = pred e
-      | otherwise = case findPath size (memories M.! m) of
+      | otherwise = case findPath size ms of
           Just _ -> go (succ m) e
           Nothing -> go s (pred m)
       where
         m = (e - s + 1) `div` 2 + s
+        ms = S.fromList $ take m memories
