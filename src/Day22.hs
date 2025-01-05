063f447e2e1f0093d6e5a78fc1b8b78d72254342
Tom Jemmett <tom.jemmett@gmail.com>
2025-01-05 22:28:05 +0000

    performance improvements for day 22 (use mutable vectors)

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,8 +1,11 @@
 module Day22 (day22, day22TestInput) where
 
 import Common
-import Data.Bits (Bits (xor, (.|.), (.&.)), shiftL)
-import Data.IntMap qualified as M
+import Control.Monad (unless)
+import Data.Bits (Bits (xor, (.&.), (.|.)), shift)
+import Data.Foldable (for_)
+import Data.Vector.Storable qualified as V
+import Data.Vector.Storable.Mutable qualified as MV
 
 day22TestInput :: String
 day22TestInput =
@@ -15,29 +18,24 @@ day22TestInput =
 day22 :: AOCSolution
 day22 input = show <$> ([part1, part2] <*> pure evolutions)
   where
-    i = parseInput input
-    !evolutions = map (take 2000 . iterate evolve) i
-
-parseInput :: String -> [Int]
-parseInput = map read . lines
+    i = map read $ lines input
+    evolutions = map (take 2000 . iterate evolve) i
 
 part1 :: [[Int]] -> Int
 part1 = sum . (!! 2000)
 
 part2 :: [[Int]] -> Int
-part2 = maximum . M.elems . foldr (M.unionWith (+) . go . map (`mod` 10)) M.empty
-  where
-    go xs = M.fromList $ reverse $ zip ks $ drop 4 xs
-      where
-        ds = zipWith (-) (tail xs) xs
-        ks = drop 4 $ scanl fn 0 ds
-          where
-            fn k x = ((shiftL k 5) .|. (x + 9)) .&. 0xFFFFF
+part2 input = V.maximum $ V.create do
+  v <- MV.replicate 0x100000 0
+  for_ (map2 (`mod` 10) input) \xs -> do
+    let ds = zipWith (-) (tail xs) xs
+        ks = scanl (\k x -> (shift k 5 .|. (x + 9)) .&. 0xFFFFF) 0 ds
+    s <- MV.replicate 0x100000 False
+    for_ (drop 4 $ zip ks xs) \(k, x) ->
+      MV.exchange s k True >>= flip unless (MV.modify v (+ x) k)
+  pure v
 
 evolve :: Int -> Int
-evolve i = c
+evolve i = foldr f i [11, -5, 6]
   where
-    a = prune $ i `xor` (i * 64)
-    b = prune $ a `xor` (a `div` 32)
-    c = prune $ b `xor` (b * 2048)
-    prune = (.&. 16777215)
+    f s x = x `xor` (x `shift` s) .&. 16777215
